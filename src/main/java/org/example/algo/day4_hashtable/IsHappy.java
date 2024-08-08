08e129b441144c797fe7813074b4a71020a46f3b
Xiaohu <yelihu.ylh@alibaba-inc.com>
2024-08-08 15:33:13 +0800

    feat(hashtable): 快乐数

---
+++ src/main/java/org/example/algo/day4_hashtable/IsHappy.java
@@ -0,0 +1,36 @@
+package org.example.algo.day4_hashtable;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ *
+ * @author yelihu
+ */
+public class IsHappy {
+    public boolean isHappy(int n) {
+        Set<Integer> record = new HashSet<>();
+        while (n != 1 && !record.contains(n)) {
+            record.add(n);
+            n = getNextNumber(n);
+        }
+        return n == 1;
+    }
+
+    private int getNextNumber(int n) {
+        int res = 0;
+        while (n > 0) {
+            int tmp = n % 10;
+            res += tmp * tmp;
+            n = n / 10;
+        }
+        return res;
+    }
+
+
+    public static void main(String[] args) {
+        IsHappy isHappy = new IsHappy();
+        System.out.println(isHappy.isHappy(19));
+
+    }
+}
